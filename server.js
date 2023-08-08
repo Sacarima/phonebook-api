2b7d605b6e79a92bd7d99c17174a8db93bcff333
Sacarima <caroline12alex@yahoo.com>
2023-08-08 16:31:11 +0200

    first commit setting up dependencies, express, server and api data

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const PORT = 4001;
+
+let persons = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "123-345-3234"
+    },
+
+    {
+        "id": 2,
+        "name": "John Atrur",
+        "number": "+244-87972-232"
+    },
+
+    {
+        "id": 3,
+        "name": "Joaa Sacarima",
+        "number": "234-8989-3"
+    },
+
+    {
+        "id": 4,
+        "name": "Luisa Maria",
+        "number": "1190-3434-232"
+    },
+
+    {
+        "id": 5,
+        "name": "Bruno king",
+        "number": "90232-232-4545"
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.listen(PORT, () => {
+    console.log(`the server is running on port ${PORT}`)
+})
