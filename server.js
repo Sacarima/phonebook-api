0f398cf867eb105e2e480c8389df15d0a0176504
Sacarima <caroline12alex@yahoo.com>
2023-08-16 23:24:50 +0200

    generate random IDs and add new entries to the phonebook

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const PORT = 4001;
 
+app.use(express.json())
+
 let persons = [
     {
         "id": 1,
@@ -61,7 +63,62 @@ app.get('/api/persons/:id', (req, res) => {
     const entry = persons.find(entry => entry.id == id)
 
     entry ? res.json(entry) : res.status(404).end()
-}) 
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(entry => entry != id)
+    res.status(204).end()
+})
+
+const generateId = () => {
+    const maxID = persons.length > 0 ? Math.max(...persons.map(n => n.id)) : 0
+    return maxID +1
+}
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    let entry = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons.push(entry)
+    res.json(entry)
+})
+
+// Generate a random ID within a specific range
+// function generateRandomId() {
+//     const min = 1000; // Minimum ID value
+//     const max = 9999; // Maximum ID value
+//     return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// app.post('/api/persons', (req, res) => {
+//     const body = req.body;
+
+//     // Check if name or number is missing
+//     if (!body.name || !body.number) {
+//         return res.status(400).json({ error: 'Name or number is missing' });
+//     }
+
+//     // Check if the name already exists in the phonebook
+//     if (persons.some(entry => entry.name === body.name)) {
+//         return res.status(400).json({ error: 'Name already exists in the phonebook' });
+//     }
+
+//     const newPerson = {
+//         id: generateRandomId(), // Generate a new ID
+//         name: body.name,
+//         number: body.number
+//     };
+
+//     persons.push(newPerson);
+//     res.json(newPerson);
+// });
+
 
 app.listen(PORT, () => {
     console.log(`the server is running on port ${PORT}`)
